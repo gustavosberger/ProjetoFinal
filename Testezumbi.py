a3c2476d4074a6cc5b7f2a039162c908055af59f
gustavosberger <gustavosberger@gmail.com>
2018-05-03 19:35:15 -0300

    Tentativa de juntar o jogador com os zumbis

--- Testezumbi.py
+++ Testezumbi.py
@@ -89,13 +89,25 @@ pygame.display.set_caption('Jogo Zumbis')
 fundo = pygame.image.load("fundo-800X600.jpg").convert()
 
 # cria os zumbis
-zumbi1 = Zumbi1("bola-25X25.png", 700, 300, 1)
+zumbi1 = Zumbi1("go_5.png", 700, 300, 1)
 zumbi1_group = pygame.sprite.Group()
 zumbi1_group.add(zumbi1)
 
-zumbi2 = Zumbi2("bola-25X25.png", 700, 300, 1.1)
+zumbi2 = Zumbi2("go_1.png", 700, 300, 1.4)
 zumbi2_group = pygame.sprite.Group()
 zumbi2_group.add(zumbi2)
+
+zumbi3 = Zumbi3("go_4.png", 700, 300, 2)
+zumbi3_group = pygame.sprite.Group()
+zumbi3_group.add(zumbi3)
+
+zumbi4 = Zumbi4("go_7.png", 700, 300, 3)
+zumbi4_group = pygame.sprite.Group()
+zumbi4_group.add(zumbi4)
+
+zumbi5 = Zumbi5("go_10.png", 700, 300, 4)
+zumbi5_group = pygame.sprite.Group()
+zumbi5_group.add(zumbi5)
 
 
 
@@ -107,10 +119,18 @@ while rodando:
     zumbi1.move()
     if zumbi1.rect.x < 0 or zumbi1.rect.x > 800:
       zumbi1.vx = - zumbi1.vx
-    if zumbi1.rect.x > 0:
-       zumbi2.move()
-       if zumbi2.rect.x < 0 or zumbi2.rect.x > 800:
-           zumbi2.vx = - zumbi2.vx
+    zumbi2.move()
+    if zumbi2.rect.x < 0 or zumbi2.rect.x > 800:
+        zumbi2.vx = - zumbi2.vx
+    zumbi3.move()
+    if zumbi3.rect.x<0 or zumbi3.rect.x > 800:
+        zumbi3.vx = -zumbi3.vx
+    zumbi4.move()
+    if zumbi4.rect.x<0 or zumbi4.rect.x > 800:
+        zumbi4.vx = -zumbi4.vx
+    zumbi5.move()
+    if zumbi5.rect.x<0 or zumbi5.rect.x > 800:
+        zumbi5.vx = -zumbi5.vx
         
     
          
@@ -120,6 +140,9 @@ while rodando:
     tela.blit(fundo, (0, 0))
     zumbi1_group.draw(tela)
     zumbi2_group.draw(tela)
+    zumbi3_group.draw(tela)
+    zumbi4_group.draw(tela)
+    zumbi5_group.draw(tela)
     pygame.display.update()      #coloca a tela na janela
     
 pygame.display.quit()
